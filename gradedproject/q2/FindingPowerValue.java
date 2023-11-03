dfc6fc6eea559456b73d5f9526881ea8231675f3
Chaitu888 <69012287+Chaitu888@users.noreply.github.com>
2023-11-03 22:59:30 +0530

    Add files via upload

---
+++ gradedproject/q2/FindingPowerValue.java
@@ -0,0 +1,32 @@
+package com.gradedproject.q2;
+
+import java.util.Scanner;
+
+public class FindingPowerValue {
+	
+	public static int power(int a, int n) {
+		
+		if(n==0) {
+			return 1;
+		}
+		int result = a*power(a, n-1);
+		
+		return result;
+	}
+
+
+	public static void main(String[] args) {
+		// importing 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the base number X : ");
+		int baseNumber = sc.nextInt();
+		System.out.println("Enter the power N : ");
+		int power = sc.nextInt();
+		
+		System.out.println(power(baseNumber, power));
+		
+		sc.close();
+	}
+
+}
